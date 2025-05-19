c2e802eed654ce246caf5ff11ad7add640459791
chauhan-09 <nsc7574@gmail.com>
2025-05-19 22:02:16 +0530

    made changes

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# name = "greeny"
+# print("Hello my name is",name)
+# print("Hello my name is " + name) #you have to provide space
+# print("Hello my name is {}".format(name))
+# print(f"Hello my name is {name}")
+
+place = input("Enter a place: ")
+place2 = input("Enter a place: ")
+emotion = input("Enter an emotion: ")
+emotion2 = input("Enter an emotion: ")
+
+s = f"Where are you right now??. I am at the {place}. ohh that's great when will you come back {place2}.Its letting late I'm {emotion}.Call me asap {emotion2}."
+print(s)
+
